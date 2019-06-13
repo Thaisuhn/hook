7a02e526d67148494d395583f8bb483f411ab59b
Tyson G <tlgreen16@gmail.com>
2019-06-13 18:22:27 -0400

    passed basic test

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,19 @@
 const express = require('express');
-const router = express.Router();
+const route = express.Router();
 const { createHmac } = require('crypto');
- 
-const PORT = 9090
-const WEBHOOK_SECRET = '<same secret as configured in Orchestrator>'
+const WEBHOOK_SECRET = process.env.SECRET;
  
 const isValidRequest = (body /* Buffer */, secret /* string */, expectedSignature /* string */) =>
-    expectedSignature == null || createHmac('sha256', secret)
-        .update(body)
-        .digest('base64') === expectedSignature
- 
-
-const server = createServer((req, resp) => {
- 
-    let body = new Buffer([])
- 
-    req.on('data', chunk => body = Buffer.concat([body, chunk]))
- 
-    req.on('end', () => {
- 
-        if (!isValidRequest(body, WEBHOOK_SECRET, req.headers['x-uipath-signature'])) {
-            console.error('Invalid signature')
-            resp.statusCode = 401 // Unauthorized
-        } else {
- 
-            let payload = JSON.parse(body.toString('utf8'))
- 
-            // Process request
-            console.log(payload)
- 
-            resp.statusCode = 202 // Accepted
-        }
- 
-        resp.end()
-    })
- 
-})
- 
-server.listen(PORT)
-
-router.get('/logs', function(req, res, next) {
-  res.send();
+    expectedSignature == WEBHOOK_SECRET;
+    
+route.post('/logs', function(req, res, next) {
+  const {body,headers} = req;
+  const valid = isValidRequest(Buffer.from(body.toString()), WEBHOOK_SECRET, headers['x-uipath-signature']);
+  if(!valid){
+  	res.sendStatus(401);
+  }else{
+  	res.sendStatus(202);
+  }
 });
-
-module.exports = router;
+ 
+module.exports = route;
--- test/route.test.js
+++ test/route.test.js
@@ -2,15 +2,30 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
 const app = require("../app");
-
 chai.use(chaiHttp);
 
 describe("/logs",(done)=>{
-	it("Should respond with 202",()=>{
-
+	it("Should respond with 202",(done)=>{
+		chai.request(app).post("/logs").set(
+			"x-uipath-signature", process.env.SECRET
+		).send({
+			"data":"here is some data"
+		}).end((err,res)=>{
+			expect(err).to.be.null;
+			expect(res).to.have.status(202);
+			done();
+		})
 	});
 
-	it("Should respond with 401",()=>{
-
+	it("Should respond with 401",(done)=>{
+		chai.request(app).post("/logs").set(
+			"x-uipath-signature", "process.env.SECRET"
+		).send({
+			"data":"here is some data"
+		}).end((err,res)=>{
+			expect(err).to.be.null;
+			expect(res).to.have.status(401);
+			done();
+		})
 	})
 });
