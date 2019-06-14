f45e9c53ee77be9015c4c561563e67c171c282f2
Tyson G <tlgreen16@gmail.com>
2019-06-14 16:45:38 -0400

    passed test for es

--- middlewear/index.js
+++ middlewear/index.js
@@ -1,16 +1,21 @@
-const {Client} = require("es6");
+const {Client} = require('@elastic/elasticsearch')
 const client = new Client({node: "http://localhost:9200"});
 const es_insert = async(data) =>{
-	if(data["EventId"] !== typeof(String) || data["EventId"] == null){
+	if(data["EventId"] == null){
 		throw new RangeError("invalid datatype for data.EventId expecting string");
 	}
+	try{
+		const result = await client.create({
+			index: process.env.INDEX,
+			id: data["EventId"],
+			type: "log",
+			body: data,
+			refresh: true
+		},{ignore:409});
+	}catch(e){
+		console.log("Im Here",e);
+	}
 
-	await client.create({
-		index:process.env.INDEX,
-		id: data["EventId"],
-		body: data
-	});
-	
 }
 
 module.exports = {
--- test/middlewear.test.js
+++ test/middlewear.test.js
@@ -4,29 +4,45 @@ const assert = chai.assert;
 const data = require("./jsonPayload");
 const {es_insert} = require("../middlewear");
 
-const {Client} = require("es6");
+const {Client} = require('@elastic/elasticsearch')
 const client = new Client({node: "http://localhost:9200"});
+
 
 describe("Web Hook MiddleWear",()=>{
 	
 	beforeEach("Creating Index For ElasticSearch",async()=>{
-		await client.indices.create({index: "default-2019.04"})
+		await client.indices.create({index: process.env.INDEX},{ignore:400})
 	});
 
-	afterEach("Removing Index From ElasticSearch",async()=>{
-		await client.indices.delete({index: "default-2019.04"})
-	})
+	afterEach("Delete Index For ElasticSearch",async()=>{
+		await client.indices.delete({index: process.env.INDEX})
+	});
+
 	// drop elasticsearch index
 	// create index if does exists
 	it("Should add data in elasticsearch", async()=>{
-		const jobCreated = data("job.created");
-		es_insert(jobCreated);
+		const jobCreated = data["job.created"];
+		await es_insert(jobCreated);
 		// verify that information exist in the database
-		const {body} = await client.search({
-			index: "default-2019.04",
-			body: {"Type":"job.created"}
-		});
+		try{
+			const {body} = await client.search({
+					index: "default-2019.04",
+					body:{
+						query:{
+							match:{
+								Type:"job.created"
+							}
+						}
+					}
+				})
+			const {hits:{total}} = body;
+			assert.equal(total,1 ,"There should be a total of 1 item in es");	
+		}catch(e){
+			console.log(e.body);
+			console.log(e.body);
+			console.log("Error",e);
+			throw e;
+		}
 
-		assert.equal(body.EventId,jobCreated.EventId, "Matching EventIds together");
 	})
 })
